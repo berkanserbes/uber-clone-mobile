c24d458f0cc3d68957005ac8e554ac9e2783ef1f
Berkan Serbes <berkan_serbes@hotmail.com>
2024-12-22 13:27:10 +0300

    feat (auth): SignUp screen layout - Refactor SignUp component to use ScrollView for better UX - Add header image and title for account creation - Implement input fields for Name, Email, and Password with icons - Include a CustomButton for the Sign Up action

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,11 +1,46 @@
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Text } from "react-native";
+import { ScrollView, Text, View, Image } from "react-native";
+import { icons, images } from "@/constants";
+import InputField from "@/components/InputField";
+import CustomButton from "@/components/CustomButton";
 
 const SignUp = () => {
   return (
-    <SafeAreaView>
-      <Text>SignUp</Text>
-    </SafeAreaView>
+    <ScrollView className="flex-1 bg-white">
+      <View className="flex-1 bg-white">
+        <View className="relative w-full h-[250px]">
+          <Image source={images.signUpCar} className="z-0 w-full h-[250px]" />
+          <Text className="text-2xl text-black font-JakartaSemiBold absolute bottom-5 left-5">
+            Create Your Account
+          </Text>
+        </View>
+
+        <View className="p-5">
+          <InputField
+            label="Name"
+            placeholder="Enter your name"
+            icon={icons.person}
+          />
+
+          <InputField
+            label="Email"
+            placeholder="Enter your email"
+            icon={icons.email}
+            textContentType="emailAddress"
+          />
+
+          <InputField
+            label="Password"
+            placeholder="Enter your password"
+            icon={icons.lock}
+            secureTextEntry={true}
+            textContentType="password"
+          />
+
+          <CustomButton title="Sign Up" className="mt-6" />
+        </View>
+      </View>
+    </ScrollView>
   );
 };
 
