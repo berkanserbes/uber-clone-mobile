7ded6b159de009523fcd6d0bd3f177d1125c47c6
Berkan Serbes <berkan_serbes@hotmail.com>
2024-12-29 12:08:46 +0300

    feat(auth): Enhance SignUp functionality with form handling and verification
    - Implemented state management for user input (name, email, password) using useState.
    - Added handleSignUp function to manage sign-up process and email verification.

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,10 +1,46 @@
 import { SafeAreaView } from "react-native-safe-area-context";
-import { ScrollView, Text, View, Image } from "react-native";
+import { ScrollView, Text, View, Image, Alert } from "react-native";
 import { icons, images } from "@/constants";
 import InputField from "@/components/InputField";
 import CustomButton from "@/components/CustomButton";
+import { useState } from "react";
+import { Link } from "expo-router";
+import OAuth from "@/components/OAuth";
+import { useSignUp } from "@clerk/clerk-expo";
 
 const SignUp = () => {
+  const { isLoaded, signUp, setActive } = useSignUp();
+
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const [verification, setVerification] = useState({
+    state: "default",
+    error: "",
+    code: "",
+  });
+
+  const handleSignUp = async () => {
+    if (!isLoaded) return;
+    try {
+      await signUp.create({
+        emailAddress: form.email,
+        password: form.password,
+        username: form.name,
+      });
+      await signUp.prepareEmailAddressVerification({ strategy: "email_code" });
+      setVerification({
+        ...verification,
+        state: "pending",
+      });
+    } catch (error: any) {
+      Alert.alert("Error", error.errors[0].longMessage);
+    }
+  };
+
   return (
     <ScrollView className="flex-1 bg-white">
       <View className="flex-1 bg-white">
@@ -19,7 +55,9 @@ const SignUp = () => {
           <InputField
             label="Name"
             placeholder="Enter your name"
+            value={form.name}
             icon={icons.person}
+            onChangeText={(value) => setForm({ ...form, name: value })}
           />
 
           <InputField
@@ -27,6 +65,8 @@ const SignUp = () => {
             placeholder="Enter your email"
             icon={icons.email}
             textContentType="emailAddress"
+            value={form.email}
+            onChangeText={(value) => setForm({ ...form, email: value })}
           />
 
           <InputField
@@ -35,9 +75,22 @@ const SignUp = () => {
             icon={icons.lock}
             secureTextEntry={true}
             textContentType="password"
+            value={form.password}
+            onChangeText={(value) => setForm({ ...form, password: value })}
           />
 
-          <CustomButton title="Sign Up" className="mt-6" />
+          <CustomButton
+            title="Sign Up"
+            className="mt-6"
+            onPress={handleSignUp}
+          />
+
+          <OAuth />
+
+          <Link href="/sign-in" className="text-base text-center mt-10">
+            Already have an account?{" "}
+            <Text className="text-primary-500">Sign In</Text>
+          </Link>
         </View>
       </View>
     </ScrollView>
