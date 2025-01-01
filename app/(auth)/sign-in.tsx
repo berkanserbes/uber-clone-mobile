58228fc8cc99328fc0ebe4378805aacb75e2a142
Berkan Serbes <berkan_serbes@hotmail.com>
2025-01-01 12:28:07 +0300

    feat(auth): Implement SignIn component with form handling and error alerts

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,11 +1,85 @@
-import { SafeAreaView } from "react-native-safe-area-context";
-import { Text } from "react-native";
+import { ScrollView, Text, View, Image, Alert } from "react-native";
+import { useSignIn } from "@clerk/clerk-expo";
+import { useCallback, useState } from "react";
+import { icons, images } from "@/constants";
+import InputField from "@/components/InputField";
+import CustomButton from "@/components/CustomButton";
+import { Link, router } from "expo-router";
+import OAuth from "@/components/OAuth";
 
 const SignIn = () => {
+  const { signIn, setActive, isLoaded } = useSignIn();
+
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleSignIn = useCallback(async () => {
+    if (!isLoaded) return;
+    try {
+      const signInAttempt = await signIn.create({
+        identifier: form.email,
+        password: form.password,
+      });
+      if (signInAttempt.status === "complete") {
+        await setActive({ session: signInAttempt.createdSessionId });
+        router.push(`/(root)/(tabs)/home`);
+      } else {
+        console.log(JSON.stringify(signInAttempt, null, 2));
+        Alert.alert("Error", "Log in failed. Please try again.");
+      }
+    } catch (error: any) {
+      console.log(JSON.stringify(error, null, 2));
+      Alert.alert("Error", error.errors[0].longMessage);
+    }
+  }, [isLoaded, form.email, form.password]);
+
   return (
-    <SafeAreaView>
-      <Text>SignIn</Text>
-    </SafeAreaView>
+    <ScrollView className="flex-1 bg-white">
+      <View className="flex-1 bg-white">
+        <View className="relative w-full h-[250px]">
+          <Image source={images.signUpCar} className="z-0 w-full h-[250px]" />
+          <Text className="text-2xl text-black font-JakartaSemiBold absolute bottom-5 left-5">
+            Welcome 👋
+          </Text>
+        </View>
+
+        <View className="p-5">
+          <InputField
+            label="Email"
+            placeholder="Enter your email"
+            icon={icons.email}
+            textContentType="emailAddress"
+            value={form.email}
+            onChangeText={(value) => setForm({ ...form, email: value })}
+          />
+
+          <InputField
+            label="Password"
+            placeholder="Enter your password"
+            icon={icons.lock}
+            secureTextEntry={true}
+            textContentType="password"
+            value={form.password}
+            onChangeText={(value) => setForm({ ...form, password: value })}
+          />
+
+          <CustomButton
+            title="Sign In"
+            className="mt-6"
+            onPress={handleSignIn}
+          />
+
+          <OAuth />
+
+          <Link href="/sign-up" className="text-base text-center mt-10">
+            Don't have an account?{" "}
+            <Text className="text-primary-500">Sign up</Text>
+          </Link>
+        </View>
+      </View>
+    </ScrollView>
   );
 };
 
