99ef60039ad61ceb01bd7f62729667bb54249e39
Berkan Serbes <berkan_serbes@hotmail.com>
2025-02-02 10:35:04 +0300

    feat(auth): Implement Google OAuth flow with user creation
    - Added Expo Linking for OAuth redirect URL
    - Integrated Google OAuth authentication with Clerk
    - Implemented user creation API call for new sign-ups
    - Enhanced error handling and success/failure response for OAuth process

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,4 +1,6 @@
 import * as SecureStorage from "expo-secure-store";
+import * as Linking from "expo-linking";
+import { fetchAPI } from "./fetch";
 
 export const tokenCache = {
   async getToken(key: string) {
@@ -29,6 +31,37 @@ export const tokenCache = {
 
 export const googleOAuth = async (startOAuthFlow: any) => {
   try {
+    const { createdSessionId, setActive, signUp } = await startOAuthFlow({
+      redirectUrl: Linking.createURL("/(root)/(tabs)/home"),
+    });
+
+    if (createdSessionId) {
+      if (setActive) {
+        await setActive({ session: createdSessionId });
+
+        if (signUp.createdUserId) {
+          await fetchAPI("/(api)/user", {
+            method: "POST",
+            body: JSON.stringify({
+              name: `${signUp.firstName} ${signUp.lastName}`,
+              email: signUp.emailAddress,
+              clerkId: signUp.createdUserId,
+            }),
+          });
+        }
+
+        return {
+          success: true,
+          code: "success",
+          message: "You have successfully signed in with Google",
+        };
+      }
+    }
+
+    return {
+      success: false,
+      message: "An error occurred while signing in with Google",
+    };
   } catch (error: any) {
     console.error("Google OAuth error: ", error);
     return {
